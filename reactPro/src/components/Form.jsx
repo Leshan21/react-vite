11db3adcfef406e11212e7f2ac01502034346cc6
Leshan21 <leshanpasindu12@gmail.com>
2025-08-24 10:13:03 +0530

    handle submition

--- reactPro/src/components/Form.jsx
+++ reactPro/src/components/Form.jsx
@@ -5,13 +5,18 @@ const Form = () => {
     firstName: "",
     lastName: ""
   });
-  
+ 
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(name);
+  }
 
   return (
     <>
       <form action="">
         <input onChange={(e) => setName({...name,firstName:e.target.value})} type="text" value={name.firstName} />
         <input onChange={(e) => setName({...name, lastName: e.target.value})} type="text" /> {/* ...name spreads the existing name object */}
+        <button onClick={(e) => handleSubmit(e)}>add</button>
       </form>
       <h1>{name.firstName} {name.lastName}</h1>
     </>
