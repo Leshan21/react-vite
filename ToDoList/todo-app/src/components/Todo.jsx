e32d5dfd1577356529e5dd0c235f5fb2e1f51514
Leshan21 <leshanpasindu12@gmail.com>
2025-08-25 18:24:44 +0530

    stop refresh after the submition

--- ToDoList/todo-app/src/components/Todo.jsx
+++ ToDoList/todo-app/src/components/Todo.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 const Todo = () => {
   const [todo, setTodo] = useState("");
+
+  function handleSubmit(e){
+    e.preventDefault(); // stop refresh after the submit
+  }
+
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
         <button type="submit">Add</button>
       </form>
