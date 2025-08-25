f68766c1068374297c8d11f55aed8cc6a8e90ef2
Leshan21 <leshanpasindu12@gmail.com>
2025-08-25 18:35:30 +0530

    submitting todo items

--- ToDoList/todo-app/src/components/Todo.jsx
+++ ToDoList/todo-app/src/components/Todo.jsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 const Todo = () => {
   const [todo, setTodo] = useState("");
 
+  const [todos, setTodos] = useState([]);
+
   function handleSubmit(e){
     e.preventDefault(); // stop refresh after the submit
+    setTodos([...todos, todo]);
+    setTodo("");
   }
 
   return (
