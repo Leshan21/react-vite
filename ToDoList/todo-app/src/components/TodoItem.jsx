a2c984746e7408f39c4aa03db28b0ff34e0b2901
Leshan21 <leshanpasindu12@gmail.com>
2025-08-27 17:55:21 +0530

    add delete button and style it

--- ToDoList/todo-app/src/components/TodoItem.jsx
+++ ToDoList/todo-app/src/components/TodoItem.jsx
@@ -4,7 +4,11 @@ const Todoitem = ({item}) => {
   return (
     <>
         <div className={style.item}>
-          <div className={style.itemName}>{item}</div>
+          <div className={style.itemName}>{item}
+          <span>
+            <button className={style.deleteButton}>X</button>
+          </span>
+          </div>
           <hr className={style.line} />
         </div>
     </>
